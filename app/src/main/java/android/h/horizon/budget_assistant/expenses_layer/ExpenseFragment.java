0341d5210b29078c56b2e219bc3ff5a763cde6e8
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-06-15 17:33:40 +0800

    Implement logging for ExpenseFragment

--- app/src/main/java/android/h/horizon/budget_assistant/expenses_layer/ExpenseFragment.java
+++ app/src/main/java/android/h/horizon/budget_assistant/expenses_layer/ExpenseFragment.java
@@ -2,6 +2,7 @@ package android.h.horizon.budget_assistant.expenses_layer;
 
 import android.h.horizon.budget_assistant.R;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +10,14 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 public class ExpenseFragment extends Fragment {
+    private static final String TAG = "ExpenseFragment";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View expenseView = inflater.inflate(R.layout.fragment_expense, container,
                 false);
+        Log.d(TAG, "onCreateView(Bundle) called");
         return expenseView;
     }
 }
