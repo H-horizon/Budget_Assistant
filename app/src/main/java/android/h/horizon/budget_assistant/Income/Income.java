446e79434b5000e632f44a425f2ffae05fc2f6e8
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-06-21 09:11:27 +0800

    Implement income classes

--- app/src/main/java/android/h/horizon/budget_assistant/Income/Income.java
+++ app/src/main/java/android/h/horizon/budget_assistant/Income/Income.java
@@ -1,2 +1,31 @@
-package android.h.horizon.budget_assistant.Income;public class Income {
+package android.h.horizon.budget_assistant.Income;
+
+import java.util.Date;
+
+public class Income {
+    protected static String Income_TITLE;
+    protected String mDescription;
+    protected float mAmount;
+    protected Date mDate;
+
+    public Income(String descriptionArg, float amountArg) {
+        mDescription = descriptionArg;
+        mAmount = amountArg;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        this.mDescription = description;
+    }
+
+    public float getAmount() {
+        return mAmount;
+    }
+
+    public void setAmount(float amount) {
+        this.mAmount = amount;
+    }
 }
--- app/src/main/java/android/h/horizon/budget_assistant/Income/Others.java
+++ app/src/main/java/android/h/horizon/budget_assistant/Income/Others.java
@@ -1,2 +1,8 @@
-package android.h.horizon.budget_assistant.Income;public class Others {
+package android.h.horizon.budget_assistant.Income;
+
+public class Others extends Income {
+    public Others(String descriptionArg, float amountArg) {
+        super(descriptionArg, amountArg);
+        Income_TITLE = "Others";
+    }
 }
