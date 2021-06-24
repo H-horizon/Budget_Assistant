0238aa5c448256df5a8938acdc46cb824c4425c9
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-06-24 10:07:29 +0800

    Make ExpensesActivity extend SingleFragment  Activity

--- app/src/main/java/android/h/horizon/budget_assistant/dashboard/ExpensesActivity.java
+++ app/src/main/java/android/h/horizon/budget_assistant/dashboard/ExpensesActivity.java
@@ -2,6 +2,7 @@ package android.h.horizon.budget_assistant.dashboard;
 
 
 import android.h.horizon.budget_assistant.R;
+import android.h.horizon.budget_assistant.abstract_classes.SingleFragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -10,51 +11,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 
-public class ExpensesActivity extends AppCompatActivity {
+public class ExpensesActivity extends SingleFragmentActivity {
     private static final String TAG = "ExpensesActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate(Bundle) called");
-        setContentView(R.layout.activity_expenses);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = fragmentManager.findFragmentById(R.id.expenses_fragment_container);
-        if (fragment == null) {
-            fragment = new ExpensesFragment();
-            fragmentManager.beginTransaction()
-                    .add(R.id.expenses_fragment_container, fragment)
-                    .commit();
-        }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
+    protected Fragment createFragment() {
+        return new ExpensesFragment();
     }
 }
