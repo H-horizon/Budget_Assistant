7f6eac48ac044f39aaf4a445c1d58c9315f745d9
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-06-24 09:26:08 +0800

    Add logging to SingleActivityFragment

--- app/src/main/java/android/h/horizon/budget_assistant/abstract_classes/SingleFragmentActivity.java
+++ app/src/main/java/android/h/horizon/budget_assistant/abstract_classes/SingleFragmentActivity.java
@@ -2,6 +2,7 @@ package android.h.horizon.budget_assistant.abstract_classes;
 
 import android.h.horizon.budget_assistant.R;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -10,6 +11,8 @@ import androidx.fragment.app.FragmentManager;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
+
+    private static String TAG = "SingleFragmentActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,35 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
                     .add(R.id.single_fragment_container, fragment)
                     .commit();
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
