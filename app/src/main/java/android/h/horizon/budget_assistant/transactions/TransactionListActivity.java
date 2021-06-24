a0900b5c04b8ed49c8075980460c0f0b484c7a48
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-06-24 09:21:55 +0800

    Move SingleFragmentActivity to new package

--- app/src/main/java/android/h/horizon/budget_assistant/transactions/TransactionListActivity.java
+++ app/src/main/java/android/h/horizon/budget_assistant/transactions/TransactionListActivity.java
@@ -1,8 +1,10 @@
 package android.h.horizon.budget_assistant.transactions;
 
+import android.h.horizon.budget_assistant.abstract_classes.SingleFragmentActivity;
+
 import androidx.fragment.app.Fragment;
 
-public class TransactionListActivity extends SingleFragmentActivity{
+public class TransactionListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new TransactionListFragment();
