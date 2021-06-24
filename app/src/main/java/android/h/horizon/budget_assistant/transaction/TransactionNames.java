068990aba50d5ef3cb1d77e2ca5eef1942d092eb
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-06-24 12:11:29 +0800

    Set title for new incomes

--- app/src/main/java/android/h/horizon/budget_assistant/transaction/TransactionNames.java
+++ app/src/main/java/android/h/horizon/budget_assistant/transaction/TransactionNames.java
@@ -1,15 +1,15 @@
 package android.h.horizon.budget_assistant.transaction;
 
 public class TransactionNames {
-    public static String FOOD = "food";
-    public static String TRAVELLING = "travelling";
-    public static String RENT = "rent";
-    public static String LEISURE = "leisure";
-    public static String EDUCATION = "education";
-    public static String HEALTH = "health";
-    public static String SUBSCRIPTION = "subscription";
-    public static String OTHER_EXPENSES = "other_expenses";
-    public static String SALARY = "salary";
-    public static String ALLOWANCE = "allowance";
-    public static String OTHER_INCOME = "other_income";
+    public static String FOOD = "Food";
+    public static String TRAVELLING = "Travelling";
+    public static String RENT = "Rent";
+    public static String LEISURE = "Leisure";
+    public static String EDUCATION = "Education";
+    public static String HEALTH = "Health";
+    public static String SUBSCRIPTION = "Subscription";
+    public static String OTHER_EXPENSES = "Other Expenses";
+    public static String SALARY = "Salary";
+    public static String ALLOWANCE = "Allowance";
+    public static String OTHER_INCOME = "Other Incomes";
 }
