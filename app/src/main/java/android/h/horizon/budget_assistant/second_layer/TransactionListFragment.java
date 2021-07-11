bc778f1eb8b5f5658f6f7ae799e5034b1ee679bc
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-07-11 17:14:14 +0800

    Enable sorting of each transaction category by time periods

--- app/src/main/java/android/h/horizon/budget_assistant/second_layer/TransactionListFragment.java
+++ app/src/main/java/android/h/horizon/budget_assistant/second_layer/TransactionListFragment.java
@@ -6,6 +6,7 @@ import android.h.horizon.budget_assistant.R;
 import android.h.horizon.budget_assistant.transaction.Transaction;
 import android.h.horizon.budget_assistant.transaction.TransactionContainer;
 import android.h.horizon.budget_assistant.third_layer.TransactionPagerActivity;
+import android.h.horizon.budget_assistant.transaction.TransactionDate;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,25 +15,37 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 /**
  * Represents the class that create a RecyclerView for a list of transactions
  */
-public class TransactionListFragment extends Fragment {
+public class TransactionListFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     private static final String ARG_TRANSACTION_TITLE = "transaction_title";
     private static final int REQUEST_CODE_TITLE = 0;
     private static final String TAG = "TransactionListFragment";
     private RecyclerView mTransactionRecyclerView;
     private TransactionAdapter mAdapter;
     private String mTransactionTitle;
+    private static final int ALL_TIME = 0;
+    private static final int TODAY = 1;
+    private static final int THIS_WEEK = 2;
+    private static final int THIS_MONTH = 3;
+    private static final int THIS_YEAR = 4;
+    private int mPosition = ALL_TIME;//default value
 
     /**
      * Passes arguments from TransactionListActivity to this class
@@ -72,8 +85,29 @@ public class TransactionListFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
         Log.d(TAG, "onCreateOptionsMenu(Menu menu, MenuInflater inflater called)");
         inflater.inflate(R.menu.fragment_transaction_list, menu);
+        MenuItem item = menu.findItem(R.id.time_spinner);
+        Spinner spinner = (Spinner) item.getActionView();
+        spinner.setOnItemSelectedListener(this);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.spinner_time_array, R.layout.spinner_items_list_main);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.d(TAG, "onItemSelected() called");
+        mPosition = position;
+        updateUI();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -83,6 +117,7 @@ public class TransactionListFragment extends Fragment {
             case R.id.menu_item_new_transaction:
                 addTransactionUsingToolbar();
                 return true;
+            case R.id.time_spinner:
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -120,16 +155,93 @@ public class TransactionListFragment extends Fragment {
     private void updateUI() {
         Log.d(TAG, "updateUI() called");
         TransactionContainer transactionContainer = TransactionContainer.get(getActivity());
-        //Update the following line to get list based on title[Delete afterwards]
         List<Transaction> transactions = transactionContainer.getTransactions(mTransactionTitle);
+        List<Transaction> filteredTransactions = new ArrayList<>();
+        filteredTransactions = getFilteredTransactionList(transactions, filteredTransactions);
+        setUI(filteredTransactions);
+    }
+
+    private List<Transaction> getFilteredTransactionList(List<Transaction> transactions,
+                                                         List<Transaction> filteredTransactions) {
+        Log.d(TAG, "updateUI() called");
+
+        switch (mPosition) {
+            case ALL_TIME:
+                Log.d(TAG, "updateUI(): ALL_TIME");
+                return transactions;
+            case TODAY:
+                setFilteredTransactionListToday(transactions, filteredTransactions);
+                break;
+            case THIS_WEEK:
+                setFilteredTransactionListThisWeek(transactions, filteredTransactions);
+                break;
+            case THIS_MONTH:
+                setFilteredTransactionListThisMonth(transactions, filteredTransactions);
+                break;
+            case THIS_YEAR:
+                setFilteredTransactionListThisYear(transactions, filteredTransactions);
+                break;
+            default:
+                Log.d(TAG, "updateUI(): UNKNOWN CONSTANT");
+        }
+        return filteredTransactions;
+    }
+
+    private void setFilteredTransactionListToday(List<Transaction> transactions,
+                                                 List<Transaction> filteredTransactions) {
+        Log.d(TAG, "setFilteredTransactionListToday() called");
+        for (Transaction transaction : transactions) {
+            Date date = new Date();
+            if (TransactionDate.isToday(date, transaction.getDate())) {
+                filteredTransactions.add(transaction);
+            }
+        }
+    }
+
+    private void setFilteredTransactionListThisWeek(List<Transaction> transactions,
+                                                    List<Transaction> filteredTransactions) {
+        Log.d(TAG, "setFilteredTransactionListThisWeek() called");
+        for (Transaction transaction : transactions) {
+            Date date = new Date();
+            if (TransactionDate.isWeekSame(date, transaction.getDate())) {
+                filteredTransactions.add(transaction);
+            }
+        }
+    }
+
+    private void setFilteredTransactionListThisMonth(List<Transaction> transactions,
+                                                     List<Transaction> filteredTransactions) {
+        Log.d(TAG, "setFilteredTransactionListThisMonth() called");
+        for (Transaction transaction : transactions) {
+            Date date = new Date();
+            if (TransactionDate.isMonthSame(date, transaction.getDate())) {
+                filteredTransactions.add(transaction);
+            }
+        }
+    }
+
+    private void setFilteredTransactionListThisYear(List<Transaction> transactions,
+                                                    List<Transaction> filteredTransactions) {
+        Log.d(TAG, "setFilteredTransactionListThisYear() called");
+        for (Transaction transaction : transactions) {
+            Date date = new Date();
+            if (TransactionDate.isYearSame(date, transaction.getDate())) {
+                filteredTransactions.add(transaction);
+            }
+        }
+    }
+
+    private void setUI(List<Transaction> filteredTransactions) {
+        Log.d(TAG, "setUI() called");
         if (mAdapter == null) {
-            mAdapter = new TransactionAdapter(transactions);
+            mAdapter = new TransactionAdapter(filteredTransactions);
             mTransactionRecyclerView.setAdapter(mAdapter);
         } else {
-            mAdapter.setTransactions(transactions);
+            mAdapter.setTransactions(filteredTransactions);
             mAdapter.notifyDataSetChanged();
         }
     }
+
 
     private void addTransactionUsingToolbar() {
         Log.d(TAG, "addTransactionUsingToolbar() called");
