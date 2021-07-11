cb26465d6f1f838795e2959c7b1b1e2574bfce37
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-07-11 13:00:30 +0800

    Allow user to donitor transactions on a weekly, monthly, yearly and all-time basis

--- app/src/main/java/android/h/horizon/budget_assistant/MainActivity.java
+++ app/src/main/java/android/h/horizon/budget_assistant/MainActivity.java
@@ -9,15 +9,25 @@ import android.h.horizon.budget_assistant.dashboard.IncomesActivity;
 import android.h.horizon.budget_assistant.transaction.Transactions;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "MainActivity";
+    public static final int ALL_TIME = 0;
+    public static final int THIS_WEEK = 1;
+    public static final int THIS_MONTH = 2;
+    public static final int THIS_YEAR = 3;
+    private int mPosition = ALL_TIME;//default value
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,35 @@ public class MainActivity extends AppCompatActivity {
         setValuesField();
         setExpensesButton();
         setIncomeButton();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu(Menu menu) called");
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        MenuItem item = menu.findItem(R.id.time_spinner);
+        Spinner spinner = (Spinner) item.getActionView();
+        spinner.setOnItemSelectedListener(this);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_time_array, R.layout.spinner_items_list_main);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        return true;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.d(TAG, "onItemSelected() called");
+        mPosition = position;
+        setValuesField();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        Log.d(TAG, "onNothingSelected() called");
     }
 
     private void setValuesField() {
@@ -67,19 +106,66 @@ public class MainActivity extends AppCompatActivity {
     private void updateUI(Transactions transactionsValues,
                           TextView revenueTextView, TextView expenditureTextView,
                           TextView savingTextView) {
-        Log.d(TAG, "updateUI(Transactions transactionsValues, TextView revenueTextView, " +
-                "TextView expenditureTextView, TextView savingTextView) called");
+        Log.d(TAG, "updateUI() called");
         DecimalFormat df = new DecimalFormat("###.##");
-        revenueTextView.setText(df.format(transactionsValues.getRevenue()));
-        expenditureTextView.setText(df.format(transactionsValues.getExpenditure()));
-        if (transactionsValues.getSavings() > 0) {
-            Log.d(TAG, "updateUI():Green");
+        double revenue;
+        double expenditure;
+        double savings;
+        if (mPosition == ALL_TIME) {
+            Log.d(TAG, "updateUI(): ALL_TIME");
+            revenue = transactionsValues.getRevenue();
+            expenditure = transactionsValues.getExpenditure();
+            savings = transactionsValues.getSavings();
+            setUI(revenueTextView, expenditureTextView, savingTextView, df, revenue, expenditure,
+                    savings);
+        } else if (mPosition == THIS_WEEK) {
+            Log.d(TAG, "updateUI(): THIS_WEEK");
+            revenue = transactionsValues.getWeeklyRevenue();
+            expenditure = transactionsValues.getWeeklyExpenditure();
+            savings = revenue - expenditure;
+            setUI(revenueTextView, expenditureTextView, savingTextView, df, revenue, expenditure,
+                    savings);
+        } else if (mPosition == THIS_MONTH) {
+            Log.d(TAG, "updateUI(): THIS_MONTH");
+            revenue = transactionsValues.getMonthlyRevenue();
+            expenditure = transactionsValues.getMonthlyExpenditure();
+            savings = revenue - expenditure;
+            setUI(revenueTextView, expenditureTextView, savingTextView, df, revenue, expenditure,
+                    savings);
+        } else if (mPosition == THIS_YEAR) {
+            Log.d(TAG, "updateUI(): THIS_YEAR");
+            revenue = transactionsValues.getYearlyRevenue();
+            expenditure = transactionsValues.getYearlyExpenditure();
+            savings = revenue - expenditure;
+            setUI(revenueTextView, expenditureTextView, savingTextView, df, revenue, expenditure,
+                    savings);
+        } else {
+            Log.d(TAG, "updateUI(): UNKNOWN CONSTANT");
+        }
+    }
+
+    private void setUI(TextView revenueTextView, TextView expenditureTextView,
+                       TextView savingTextView, DecimalFormat df, double revenue,
+                       double expenditure, double savings) {
+        Log.d(TAG, "setUI() called");
+        setUiText(revenueTextView, expenditureTextView, savingTextView, df, revenue, expenditure,
+                savings);
+        savingTextView.setText(df.format(savings));
+    }
+
+    private void setUiText(TextView revenueTextView, TextView expenditureTextView,
+                           TextView savingTextView, DecimalFormat df, double revenue,
+                           double expenditure, double savings) {
+        Log.d(TAG, "setUiText() called");
+        revenueTextView.setText(df.format(revenue));
+        expenditureTextView.setText(df.format(expenditure));
+        if (savings > 0) {
+            Log.d(TAG, "setUiText():Green");
             savingTextView.setTextColor(Color.parseColor("#01DEFA"));
         } else {
             savingTextView.setTextColor(Color.parseColor("#DC0000"));
-            Log.d(TAG, "updateUI():Red");
+            Log.d(TAG, "setUiText():Red");
         }
-        savingTextView.setText(df.format(transactionsValues.getSavings()));
     }
 
     @Override
@@ -112,5 +198,4 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.d(TAG, "onDestroy() called");
     }
-
 }
---
+++ app/src/main/java/android/h/horizon/budget_assistant/transaction/TransactionDate.java
@@ -0,0 +1,82 @@
+package android.h.horizon.budget_assistant.transaction;
+
+import android.util.Log;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * This class consists of method that can be used to retrieve information for sorting
+ * on transactions' dates
+ */
+public class TransactionDate {
+    private static final String TAG = "TransactionDate";
+
+    /**
+     * checks whether or not both parameters are on the same week
+     *
+     * @param searchValue is the first date
+     * @param targetValue is the second date
+     * @return true if consider is satisfied
+     */
+    public static boolean isWeekSame(Date searchValue, Date targetValue) {
+        Log.d(TAG, "isWeekSame(Date searchValue, Date targetValue) called");
+        if (isMonthSame(searchValue, targetValue)) {
+            return getWeek(searchValue) == getWeek(targetValue);
+        }
+        return false;
+    }
+
+    /**
+     * checks whether or not both parameters are on the same month
+     *
+     * @param searchValue is the first date
+     * @param targetValue is the second date
+     * @return true if consider is satisfied
+     */
+    public static boolean isMonthSame(Date searchValue, Date targetValue) {
+        Log.d(TAG, "isMonthSame(Date searchValue, Date targetValue) called");
+        if (isYearSame(searchValue, targetValue)) {
+            return getMonth(searchValue) == getMonth(targetValue);
+        }
+        return false;
+    }
+
+    /**
+     * checks whether or not both parameters are on the same year
+     *
+     * @param searchValue is the first date
+     * @param targetValue is the second date
+     * @return true if consider is satisfied
+     */
+    public static boolean isYearSame(Date searchValue, Date targetValue) {
+        Log.d(TAG, "isYearSame(Date searchValue, Date targetValue) called");
+        return getYear(searchValue) == getYear(targetValue);
+    }
+
+    private static int getWeek(Date date) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        return calendar.get(Calendar.WEEK_OF_YEAR);
+    }
+
+    private static int getMonth(Date date) {
+        Log.d(TAG, "getMonth(Date date) called");
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            return localDate.getMonthValue();// returns value from 0 to 11
+        }
+        Log.d(TAG, "getMonth(Date date): Outdated OS");
+        return -1;
+    }
+
+    private static int getYear(Date date) {
+        Log.d(TAG, "getYear(Date date) called");
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        return calendar.get(Calendar.YEAR);
+    }
+}
